e862fa15b188149c20e288e10db84ee687b6b009
Boltlord1 <boltlord1@gmail.com>
2025-11-13 08:07:38 +0500

    Created function to call the api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './styles.css'
+
+async function getData(city) {
+    const link = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}
+        ?unitGroup=us&include=days&key=TP5NVFQL2P2EWLFRWFK7XSBRN&contentType=json`
+    const data = await fetch(link)
+    const json = await data.json()
+    return json
+}
+
+const button = document.querySelector('button')
+button.addEventListener('click', async () => {
+    let city = prompt('Enter a city:', 'London')
+    const data = await getData(city)
+    
+})
--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -11,6 +12,11 @@ module.exports = {
   devServer: {
     watchFiles: ["./src/template.html"],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
   module: {
     rules: [
       {
